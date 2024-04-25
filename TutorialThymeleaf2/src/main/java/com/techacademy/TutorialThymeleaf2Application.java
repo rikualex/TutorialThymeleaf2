29ea53e4928de9a6419cf270bed51e3e847a01cd
dirFY-24hosodarikuto <rikuto.hosoda.b926@dirbato.co.jp>
2024-04-25 10:21:03 +0900

    lesson20 最終

---
+++ TutorialThymeleaf2/src/main/java/com/techacademy/TutorialThymeleaf2Application.java
@@ -0,0 +1,13 @@
+package com.techacademy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TutorialThymeleaf2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TutorialThymeleaf2Application.class, args);
+	}
+
+}
